9613bc7048794d445c3c6192f5d20f490f08bc65
Ben Carlson <benocarlson@gmail.com>
2020-02-22 17:37:19 -0700

    Edit javascript file

--- pages/recipe-search/r-s-script.js
+++ pages/recipe-search/r-s-script.js
@@ -9,5 +9,29 @@ document.getElementById("recipe-submit").addEventListener("click", function(even
       return response.json();
     }).then(function(json) {
       console.log(json);
+
+      let results = "";
+      if (json.meals === null || json.meals.length === 0) {
+        results += '<h3>No Search Results for "' + input + '"</h3>';
+      } else {
+        results += '<h3>Search Results for "' + input + '"</h3>"';
+
+        for (let meal of json.meals) {
+          results += "<div class='search-result' id='meal-" + meal.idMeal + "'>";
+          results += "<h4>" + meal.strMeal + "</h4>";
+          results += "<p>" + meal.strCategory + "</p>";
+          results += "</div>";
+        }
+      }
+      document.getElementById("recipe-search-results").innerHTML = results;
+
+      if (json.meals != null) {
+        for (let meal of json.meals) {
+          document.getElementById("meal-" + meal.idMeal).addEventListener("click", function(event) {
+            event.preventDefault();
+            // TODO build recipe html
+          })
+        }
+      }
     });
 });
